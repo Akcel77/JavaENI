de7c773b7de4293d07d3f8b18605b8fa505d1242
Akcel77 <diagneaxel@contact.com>
2021-05-18 18:29:49 +0200

    TP Swing P6 IHM OBserver

---
+++ papeterieJdbcp6Swing/src/fr/eni/papeterie/ihm/IntPanelBtnObserver.java
@@ -0,0 +1,17 @@
+package fr.eni.papeterie.ihm;
+
+
+
+public interface IntPanelBtnObserver {
+
+    void precedent();
+
+    void suivant();
+
+    void nouveau();
+
+    void enregistrer();
+
+    void supprimer();
+
+}
